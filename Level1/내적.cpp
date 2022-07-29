418d49215af3cea57775ffed0114b6faee310fa8
YongsulLee <sadghnmb04@gmail.com>
2022-07-29 12:27:00 +0900

    Feature: add Level1, 내적

---
+++ Level1/내적.cpp
@@ -0,0 +1,17 @@
+// 민망할 정도로 쉬운 문제...
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b)
+{
+    int answer = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        answer += a[i] * b[i];
+    }
+
+    return answer;
+}
