a440cc6e403123aac903131617ee0e8b3f5b5b9c
Yongsullee <webdev_ys@kakao.com>
2022-09-13 21:26:01 +0900

    Feature: add Level 1, to_string() 활용 자릿수 더하기

---
+++ Level1/자릿수더하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    string str = to_string(n);
+    
+    for(int i=0; i<str.size(); i++)
+    {
+        int num = str[i] - '0';
+        
+        answer += num;
+    }
+
+    return answer;
+}
