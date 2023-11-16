d3e1f0534fa7d10361b470b874e04b53a2e2ba76
Yongsullee <webdev_ys@kakao.com>
2023-11-16 23:12:03 +0900

    Feature: Level2, 정수 삼각형, DP 활용 Bottom-Up 연산

---
+++ Level2/정수 삼각형.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    
+    for(int i=triangle.size()-1; i>0; i--)
+    {
+        for(int j=0; j<i; j++)
+        {
+            triangle[i-1][j] += max(triangle[i][j], triangle[i][j+1]);
+        }
+    }
+    
+    answer = triangle[0][0];
+    
+    return answer;
+}
