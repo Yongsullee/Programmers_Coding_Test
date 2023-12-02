b82f113ded9ace5d9d6ca49066a651b430c50eac
YongsulLee <sadghnmb04@gmail.com>
2023-12-02 21:18:26 +0900

    Feature : add Level2, 구명보트, 투 포인터 활용 문제

---
+++ Level2/구명보트.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    
+    // 오름차순 정렬
+    sort(begin(people), end(people));
+    
+    // 투 포인터
+    int left = 0;
+    int right = people.size()-1;
+    
+    while(left <= right)
+    {
+        // 남아 있는 인원 중 가장 가벼운 인원과 가장 무거운 인원이 1개의 구명보트에 함께 탈수 있는지 체크합니다.
+        if(people[left] + people[right] <= limit)
+        {
+            // #!. 2명이 1개의 보트에 함께 탈 수 있다면, left 포인터를 오른쪽으로 한 칸 이동합니다.
+            left++;
+        }
+        // #2. right 포인터를 왼쪽으로 한 칸 이동시킵니다.
+        right--;
+        answer++;
+    }
+    
+    return answer;
+}
